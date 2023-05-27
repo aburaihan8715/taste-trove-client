b767ab72d4cacf85780babd399fb1e1d0e35b3a6
aburahan8721 <aburaihan8721@gmail.com>
2023-05-27 15:43:10 +0600

    dynamic chef recipes done

--- src/components/ChefRecipes/ChefRecipes.jsx
+++ src/components/ChefRecipes/ChefRecipes.jsx
@@ -5,24 +5,9 @@ const ChefRecipes = () => {
   const params = useParams();
   const data = useLoaderData();
   const chefRecipes = data?.find((chefRecipe) => chefRecipe.id === params.id);
-  const { image, chefName, bio, likes, numberOfRecipes, yearsOfExperience, recipes} = chefRecipes;
+  const { image, chefName, bio, likes, numberOfRecipes, yearsOfExperience, recipes } = chefRecipes;
 
-  // ingredient items
-  const ingredientItems = recipes[0]?.ingredients?.map((item, index) => (
-    <p key={index}>
-      ({index + 1}). {item}
-    </p>
-  ));
-
-  // cooking steps
-  const cookingSteps = recipes[0]?.cookingSteps?.map((item, index) => (
-    <p key={index}>
-      ({index + 1}). {item}
-    </p>
-  ));
-
-  const ratings=recipes[0]?.rating
-
+  
   return (
     <div className="py-8">
       <div className="container mx-auto">
@@ -45,26 +30,40 @@ const ChefRecipes = () => {
         </div>
 
         <hr className="my-8" />
-        <div className="">
-          <h2 className="text-2xl capitalize font-medium text-center">{recipes[0]?.recipeName}</h2>
-          <div className="mt-8">
-            <h3 className="font-semibold">🥝🍓 Ingredients :</h3>
-            {ingredientItems}
-          </div>
-        </div>
+        <div>
+          {recipes.map((recipe) => (
+            <div className="mb-8">
+              <div className="">
+                <h2 className="text-2xl capitalize font-medium text-center">{recipe?.recipeName}</h2>
+                <div className="mt-8">
+                  <h3 className="font-semibold">🥝🍓 Ingredients :</h3>
+                  {recipe?.ingredients?.map((item, index) => (
+                    <p key={index}>
+                      ({index + 1}). {item}
+                    </p>
+                  ))}
+                </div>
+              </div>
 
-        <div className="mt-8">
-          <div>
-            <h3 className="font-semibold">👉 Cooking Steps :</h3>
-            {cookingSteps}
-          </div>
-        </div>
+              <div className="mt-8">
+                <div>
+                  <h3 className="font-semibold">👉 Cooking Steps :</h3>
+                  {recipe?.cookingSteps?.map((item, index) => (
+                    <p key={index}>
+                      ({index + 1}). {item}
+                    </p>
+                  ))}
+                </div>
+              </div>
 
-        <div className=" flex justify-end">
-          <div className="text-2xl border p-2 space-x-2">
-            <span >⭐⭐⭐⭐ : {ratings}</span>
-            <button>🧡</button>
-          </div>
+              <div className=" flex justify-end">
+                <div className="text-2xl border p-2 space-x-2">
+                  <span>⭐⭐⭐⭐ : {recipe?.rating}</span>
+                  <button>🧡</button>
+                </div>
+              </div>
+            </div>
+          ))}
         </div>
       </div>
     </div>
