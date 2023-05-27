8d47a7bcf626decdacf6b6d79d45232eb528dff5
aburahan8721 <aburaihan8721@gmail.com>
2023-05-27 18:31:04 +0600

    remove unused files

--- src/components/Blogs/Blogs.jsx
+++ src/components/Blogs/Blogs.jsx
@@ -1,31 +1,7 @@
-import React, { useContext, useEffect, useState } from "react";
-import { AuthContext } from "../../contexts/AuthProvider";
-import LoadingSpinner from "../common/LoadingSpinner/LoadingSpinner";
+import React from "react";
 
 const Blogs = () => {
-  const [data, setData] = useState([]);
-  const { loading, setLoading } = useContext(AuthContext);
-  console.log(data);
-
-  useEffect(() => {
-    fetch("http://localhost:4000/users")
-      .then((res) => res.json())
-      .then((data) => {
-        setData(data);
-        setLoading(false)
-      });
-  }, []);
-
-  if (loading) {
-    return <LoadingSpinner></LoadingSpinner>;
-  }
-
-  return (
-    <div className="container mx-auto">
-      <h2>length:{data?.length}</h2>
-      blogs
-    </div>
-  );
+  return <div className="container mx-auto">blogs</div>;
 };
 
 export default Blogs;
